d9557c25f4a71ef169b6ca503f9f9c227186a5b0
miles dustin <milesdustin45@gmail.com>
2025-12-07 13:25:59 -0800

    moved to argparse over getopt

--- aslicer.py
+++ aslicer.py
@@ -1,4 +1,5 @@
-import getopt
+import argparse
+import argcomplete
 import sys
 import os
 from pathlib import Path
@@ -39,103 +40,77 @@ def main():
     output_directory = None
     threshold = 3.0
 
-    opts, args = getopt.getopt(
-        sys.argv[1:],
-        "hit:o:d",
-        ["help", "keep-intro", "threshold=", "output=", "to-dir"],
-    )
+    parser = argparse.ArgumentParser(description="audio slicer tool")
+    parser.add_argument("-t", "--threshold", type=float, default=3.0, help="set threshold for transient detection")
+    parser.add_argument("-i", "--keep-intro", action="store_true", help="treat beginning of file as transient")
+    parser.add_argument("-o", "--output", type=str, help="write audio slices to directory (implies -d)")
+    parser.add_argument("-d", "--to-dir", action="store_true", help="write audio slices to directory named after file")
+    parser.add_argument("-f", "--format", type=str, default="wav", help="format of sliced audio clips")
+    parser.add_argument("files", nargs="+", help="audio files to slice")
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
 
-    # Process options
-    for o, a in opts:
-        if o in ["-h", "--help"]:
-            help(0)
-        elif o in ["-i", "--keep-intro"]:
-            keep_intro = True
-        elif o in ["-t", "--threshold"]:
-            try:
-                threshold = float(a)
-            except ValueError:
-                raise Exception("threshold must be a valid float")
-            if threshold <= 0:
-                raise Exception("threshold value must be positive")
-        elif o in ["-o", "--output"]:
-            if os.path.exists(a) and (not os.path.isdir(a) or not os.access(a, os.W_OK)):
-                raise Exception(f"cannot create output directory '{a}'")
-            else:
-                output_directory = a
-        elif o in ["-d", "--to-dir"]:
-            if not output_directory:
-                output_directory = ""
-        else:
-            help(1)
+    try:
+        files = []
+        for a in args.files:
+            p = Path(a)
+            if not p.exists():
+                raise Exception(f"file '{a}' does not exist")
+            files.append((a, a[0 : -len(p.suffix)], p.suffix.strip(".")))
 
-    # Check arguments
-    if len(args) == 0:
-        help(1)
+        for f in files:
+            audio = AudioSegment.from_file(f[0], f[2])
+            transients = []
 
-    files = []
-    for a in args:
-        p = Path(a)
-        if not p.exists():
-            raise Exception(f"file '{a}' does not exist")
-        files.append((a, a[0 : -len(p.suffix)], p.suffix.strip(".")))
+            if args.keep_intro:
+                transients.append(0)
 
-    for f in files:
-        audio = AudioSegment.from_file(f[0], f[2])
-        transients = []
+            baseline = audio[0].rms
+            a = 0.95
+            maxDeriv = 0.01
+            cooldown = 10
+            cool = 0
 
-        if keep_intro:
-            transients.append(0)
+            for i in range(1, len(audio)):
+                baseline = a * baseline + (1 - a) * audio[i].rms
+                ratio = audio[i].rms / (baseline + 1e-9)
+                deriv = audio[i].rms - audio[i - 1].rms
+                if cool == 0:
+                    if ratio > threshold and deriv > maxDeriv:
+                        transients.append(i)
+                        cool = cooldown
+                        print(
+                            f"file: {f[0]:<30}transients: {len(transients)}",
+                            end="\r",
+                            file=sys.stderr,
+                        )
+                else:
+                    cool -= 1
+            print("")
 
-        baseline = audio[0].rms
-        a = 0.95
-        maxDeriv = 0.01
-        cooldown = 10
-        cool = 0
+            if len(audio) - 1 not in transients:
+                transients.append(len(audio) - 1)
 
-        for i in range(1, len(audio)):
-            # calculate baseline
-            baseline = a * baseline + (1 - a) * audio[i].rms
-            # update energy ratio
-            ratio = audio[i].rms / (baseline + 1e-9)
-            # derivative for calculating change
-            deriv = audio[i].rms - audio[i - 1].rms
+            directory = f[1] if (args.to_dir and args.output is None) else args.output
+            if directory and not os.path.exists(directory):
+                os.mkdir(directory)
 
-            if cool == 0:
-                if ratio > threshold and deriv > maxDeriv:
-                    transients.append(i)
-                    cool = cooldown
-                    print(
-                        f"file: {f[0]:<30}transients: {len(transients)}",
-                        end="\r",
-                        file=sys.stderr,
-                    )
-            else:
-                cool -= 1
-        print("")
-        if len(audio) - 1 not in transients:
-            transients.append(len(audio) - 1)
-
-        directory = f[1] if output_directory == "" else output_directory
-        if directory and not os.path.exists(directory):
-            os.mkdir(directory)
-
-        for i in range(1, len(transients)):
-            seg = audio[transients[i - 1] : transients[i]]
-            seg.export(
-                "{}{}_{}.{}".format(
-                    (directory + "/" if directory else ""),
-                    f[1],
-                    i,
-                    output_format,
-                ),
-                output_format,
-            )
+            for i in range(1, len(transients)):
+                seg = audio[transients[i - 1] : transients[i]]
+                seg.export(
+                    "{}{}_{}.{}".format(
+                        (directory + "/" if directory else ""),
+                        f[1],
+                        i,
+                        args.format,
+                    ),
+                    args.format,
+                )
+    except Exception as e:
+        print(f"failure: {e}", file=sys.stderr)
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        print(f"error: {e}")
-        help(1)
+    main()
